875fe5be237564f32f404e6058430ec684abcd08
Shaii Ong <monsteronfire@gmail.com>
2017-09-02 01:12:33 +0800

    Add first dummy test

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-var Calculator = (function () {
-  var display = document.querySelector('input[name="display"]');
+//var Calculator = (function () {
+  //var display = document.querySelector('input[name="display"]');
 
-  function generateButtons() {
-    var btnValues = ['+', '-', '*', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    var allButtons = [];
+  //function generateButtons() {
+    //var btnValues = ['+', '-', '*', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    //var allButtons = [];
 
-    for(var i = 0; i < btnValues.length; i++) {
-      allButtons.push(document.querySelector('button[value="' + btnValues[i] + '"]'));
-    }
+    //for(var i = 0; i < btnValues.length; i++) {
+      //allButtons.push(document.querySelector('button[value="' + btnValues[i] + '"]'));
+    //}
 
-    return allButtons;
-  }
+    //return allButtons;
+  //}
 
-  function addClickHandlers() {
-    document.querySelector('button[value="="]').addEventListener('click', calculateEquation);
-    document.querySelector('button[value="C"]').addEventListener('click', clearAll);
+  //function addClickHandlers() {
+    //document.querySelector('button[value="="]').addEventListener('click', calculateEquation);
+    //document.querySelector('button[value="C"]').addEventListener('click', clearAll);
 
-    generateButtons().forEach(function(btn) {
-      btn.addEventListener('click', printValue);
-    });
-  }
+    //generateButtons().forEach(function(btn) {
+      //btn.addEventListener('click', printValue);
+    //});
+  //}
 
-  function printValue(event) {
-    event.preventDefault();
-    display.value += this.value;
-  }
+  //function printValue(event) {
+    //event.preventDefault();
+    //display.value += this.value;
+  //}
 
-  function calculateEquation(event) {
-    event.preventDefault();
-    display.value = eval(display.value);
-  }
+  //function calculateEquation(event) {
+    //event.preventDefault();
+    //display.value = eval(display.value);
+  //}
 
-  function clearAll(event) {
-    event.preventDefault();
-    display.value = '';
-  }
+  //function clearAll(event) {
+    //event.preventDefault();
+    //display.value = '';
+  //}
 
-  return {
-    init: addClickHandlers
-  };
+  //return {
+    //init: addClickHandlers
+  //};
 
-})();
+//})();
---
+++ test/scriptTest.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+var script = require('../script');
+
+describe('script', function () {
+  it('should pass', function () {
+    assert.equal(true, true);
+  });
+});
