723842830a148368b6d0d80d977d4d598a5f1f54
Shaii Ong <monsteronfire@gmail.com>
2017-09-03 00:37:08 +0800

    Use Jasmine instead

--- test/scriptTest.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert;
-var script = require('../script');
-
-describe('script', function () {
-  it('should pass', function () {
-    assert.equal(true, true);
-  });
-});
